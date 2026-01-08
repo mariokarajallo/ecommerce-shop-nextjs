ec66d2ff570c3aae567b088e740f03a4c689eac0
Mario Karajallo <mariokarajallo92@gmail.com>
2026-01-08 04:12:39 -0300

    feat: Correct redirection of product categories.

--- src/app/(shop)/category/[id]/page.tsx
+++ src/app/(shop)/category/[id]/page.tsx
@@ -1,8 +1,13 @@
+import { ProductGrid, Title } from "@/components";
+import { initialData } from "@/seed/seed";
 import { notFound } from "next/navigation";
+import { Category } from "@/interfaces/product.interface";
+
+const seeProducts = initialData.products;
 
 interface Props {
     params: {
-        id: string
+        id: Category
     }
 }
 
@@ -10,13 +15,22 @@ export default async function CategoryPage({ params }: Props) {
 
     const {id} = await params;
 
-    if ( id === 'kids') {
-     notFound();   
-    }
+    const productsByGender = seeProducts.filter(product => product.gender === id);
+
+const labels: Record<Category, string> ={
+    'men': 'para Hombres',
+    'women': 'para Mujeres',
+    'kid': ' para Niños',
+    'unisex': 'para todos'
+}
+    // if ( id === 'kids') {
+    //  notFound();   
+    // }
 
     return (
-        <div>
-            <h1>Category {id}</h1>
-        </div>
+         <>
+              <Title title={`Articulos de ${labels[id]}`} subtitle='Todos los productos' className="mb-2" />
+              <ProductGrid products={productsByGender} />
+            </>
     )
 }
