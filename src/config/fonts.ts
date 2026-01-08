4a365c2c2d4406c0748970f9f82579be8ede1bef
Mario Karajallo <mariokarajallo92@gmail.com>
2026-01-07 22:13:53 -0300

    Init

---
+++ src/config/fonts.ts
@@ -0,0 +1,12 @@
+import { Inter, Montserrat_Alternates } from "next/font/google";
+
+
+export const inter = Inter({
+  subsets: ["latin"],
+});
+
+export const titleFont = Montserrat_Alternates({
+  weight: ['500', '700'],
+  subsets: ["latin"],
+});
+
