c9e909b8a4e41e83d0567b034255a16ed2aa369f
Mario Karajallo <mariokarajallo92@gmail.com>
2026-01-12 12:50:16 -0300

    feat: create page cart

--- src/app/(shop)/cart/page.tsx
+++ src/app/(shop)/cart/page.tsx
@@ -1,7 +1,55 @@
+import { QuantitySelector, Title } from "@/components";
+import { initialData } from "@/seed/seed";
+import Link from "next/link";
+import Image from "next/image";
+
+const productsInCart = [
+    initialData.products[0],
+    initialData.products[1],
+    initialData.products[2],
+]
+
+
 export default function CartPage() {
     return (
+       <div className="flex justify-center items-center mb-72 px-10 sm:px-0">
+           <div className="flex flex-col w-[1000px]">
+            <Title title="Carrito de compras" />
+
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
+{/* carrito */}
+<div className="flex flex-col mt-5">
+    <span className="text-xl"> Agregar más items</span>
+    <Link 
+    className="underline mb-5"
+    href="/">Continuar comprando</Link>
+</div>
+
+{/* Items del carrito */}
+{productsInCart.map(product => (
+    <div key={product.slug} className="flex">
+        <Image
+        src={`/products/${product.images[0]}`}
+        alt={product.title}
+        width={100}
+        height={100}
+        className="mr-5 rounded"
+        />
         <div>
-            <h1>Cart</h1>
+            <p>{product.title}</p>
+            <p>$ {product.price}</p>
+            <QuantitySelector quantity={3} />
+
+            <button className="underline mt-3">Remover</button>
         </div>
+    </div>
+))}
+
+
+
+{/* checkout */}
+            </div>
+           </div>
+       </div>
     )
 }
