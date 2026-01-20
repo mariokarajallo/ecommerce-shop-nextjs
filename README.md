2d08dc45f389caa52aec6378a645a353e5b10842
Mario Karajallo <mariokarajallo92@gmail.com>
2026-01-20 18:26:07 -0300

    create schema model

--- README.md
+++ README.md
@@ -6,6 +6,8 @@
 2.  crear una copia del archivo .env.example y nombrarlo .env y cambiar los valores de las variables de entorno
 3.  instalar dependencias `npm install`
 4.  levantar la base de datos `docker compose up -d`
-5.  iniciar el servidor `npm run dev`
+5.  ejecutar las migraciones `npx prisma migrate dev`
+
+6.  iniciar el servidor `npm run dev`
 
 ## correr en PROD
---
+++ prisma/migrations/20260120193718_product_category/migration.sql
@@ -0,0 +1,41 @@
+-- CreateEnum
+CREATE TYPE "Size" AS ENUM ('XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL');
+
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('men', 'women', 'kid');
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT NOT NULL,
+    "inStock" INTEGER NOT NULL,
+    "price" DOUBLE PRECISION NOT NULL DEFAULT 0,
+    "sizes" "Size"[] DEFAULT ARRAY[]::"Size"[],
+    "slug" TEXT NOT NULL,
+    "tags" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "gender" "Gender" NOT NULL,
+    "categoryId" TEXT,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Category" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Category_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Product_slug_key" ON "Product"("slug");
+
+-- CreateIndex
+CREATE INDEX "Product_gender_idx" ON "Product"("gender");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Category_name_key" ON "Category"("name");
+
+-- AddForeignKey
+ALTER TABLE "Product" ADD CONSTRAINT "Product_categoryId_fkey" FOREIGN KEY ("categoryId") REFERENCES "Category"("id") ON DELETE SET NULL ON UPDATE CASCADE;
